ac91ff3bec4ec24652ae5c00a0fefa3dba0a73b8
misaki12dev <misaki.12k@gmail.com>
2022-01-16 12:00:10 +0900

    スタイル調整&js実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// = require jquery
+// = require jquery_ujs
+require('common')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
